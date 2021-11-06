57055677236bb0d7c821e6cd6bf9bde2909fae9c
An <chenguangandrew@gmail.com>
2021-11-06 17:21:55 +0100

    export results

--- videoJnd/videoJnd/admin.py
+++ videoJnd/videoJnd/admin.py
@@ -8,6 +8,9 @@ admin.site.site_title = "JND Video Study"
 admin.site.index_title = "Admin"
 import csv
 import time
+import io
+import zipfile
+import pandas as pd
 
 from videoJnd.src.CreateVideosObj import createEncodedRefVideosDB
 
@@ -32,26 +35,60 @@ class ExperimentAdmin(admin.ModelAdmin):
         super(ExperimentAdmin, self).save_model(request, obj, form, change)
         createEncodedRefVideosDB(obj)
 
-    # def export_result(self, request, queryset):
-    #     try:
-    #         csv_name = "jnd_video_result_" + time.strftime("%Y-%m-%d_%H-%M-%S")
-    #         meta = VideoObj._meta
-    #         column_names = [field.name for field in meta.fields if field.name not in ["id"]]
-    #         response = HttpResponse(content_type='text/csv')
-    #         response['Content-Disposition'] = 'attachment; filename={}.csv'.format(csv_name)
-    #         writer = csv.writer(response)
-    #         writer.writerow(column_names)
+    def export_result(self, request, queryset):
+        try:
+            result_df = {}
 
-    #         for obj in queryset:
-    #             videos = VideoObj.objects.filter(exp=obj)
-    #             for v in videos:
-    #                 writer.writerow([getattr(v, field) for field in column_names])
+            e_column_names = [
+                f.name for f in Experiment._meta.get_fields() \
+                    if f.name not in [
+                        "encodedrefvideoobj", "participant", "assignment"
+                    ]
+            ] # experiment
+            v_column_names = [f.name for f in EncodedRefVideoObj._meta.get_fields()] # reference video
+            p_column_names = [f.name for f in Participant._meta.get_fields()] # participant
+            a_column_names = [f.name for f in Assignment._meta.get_fields()] # assignment
 
-    #         return response
-    #     except Exception as e:
-    #         print("admin page got error: " + str(e))
 
-    # export_result.short_description = "Export Selected"
+            print(e_column_names)
+
+            for exp_obj in queryset:
+                exp_name = exp_obj.name
+
+                v_objs = EncodedRefVideoObj.objects.filter(exp=exp_obj)
+                p_objs = Participant.objects.filter(exp=exp_obj)
+                a_objs = Assignment.objects.filter(exp=exp_obj)
+
+                e_data = [tuple([getattr(exp_obj, c) for c in e_column_names])]
+                v_data = [tuple([getattr(v, c) for c in v_column_names]) for v in v_objs]
+                p_data = [tuple([getattr(p, c) for c in p_column_names]) for p in p_objs]
+                a_data = [tuple([getattr(a, c) for c in a_column_names]) for a in a_objs]
+
+                e_df = pd.DataFrame(e_data, columns = e_column_names) 
+                v_df = pd.DataFrame(v_data, columns = v_column_names) 
+                p_df = pd.DataFrame(p_data, columns = p_column_names)
+                a_df = pd.DataFrame(a_data, columns = a_column_names)
+
+                result_df[f"{exp_name}_config"] = e_df
+                result_df[f"{exp_name}_refvideo"] = v_df
+                result_df[f"{exp_name}_participant"] = p_df
+                result_df[f"{exp_name}_assignment"] = a_df
+
+                buffer = io.BytesIO()
+                with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as zf:
+                    for k, df in result_df.items():
+                        f_name = "%s_%s.csv" % (k, time.strftime("%Y-%m-%d_%H-%M-%S"))
+                        zf.writestr(f_name, df.to_csv(index=False))
+                
+                response = HttpResponse(buffer.getvalue(), content_type='application/zip')
+                response['Content-Disposition'] = 'attachment; filename={}.zip'.format("videoJND_Result_%s" % \
+                                                                     (time.strftime("%Y-%m-%d_%H-%M-%S")))
+                return response
+
+        except Exception as e:
+            print("admin page got error: " + str(e))
+
+    export_result.short_description = "Export Selected"
 
 @admin.register(EncodedRefVideoObj)
 class EncodedRefVideoObjAdmin(admin.ModelAdmin):
